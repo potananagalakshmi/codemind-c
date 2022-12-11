ee36d4a994fb96b52765036e28f989fd33918add
potananagalakshmi <119916338+potananagalakshmi@users.noreply.github.com>
2022-12-11 22:24:15 +0530

    #include<stdio.h> int main() {     int n,rem,sum=0,i;     scanf("%d",&n);     for(i=1;i<=(n-1);i++)     {         rem=n%i;         if(rem==0)         {             sum=sum+i;         }     }     if(sum==n)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Perfect_Number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,rem,sum=0,i;
+    scanf("%d",&n);
+    for(i=1;i<=(n-1);i++)
+    {
+        rem=n%i;
+        if(rem==0)
+        {
+            sum=sum+i;
+        }
+    }
+    if(sum==n)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
