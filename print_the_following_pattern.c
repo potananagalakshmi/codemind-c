b29372483845954003e26a2d940c376fa2f6ca02
potananagalakshmi <119916338+potananagalakshmi@users.noreply.github.com>
2023-01-24 21:00:53 +0530

    #include<stdio.h> int main() {     int r,o,i1,i2;     int n=1;     scanf("%d",&r);     for(o=0;o<r;o++)     {         for(i1=r-1;i1>o;i1--)         {             printf(" ");         }         for(i2=0;i2<n;i2++)         {             printf("%d",n-o);
            }
            n+=2; 
            printf("
    ");
        }
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,16 +1,22 @@
 #include<stdio.h>
 int main()
 { 
-    int i,n,j;
-    scanf("%d",&n); 
-    for(i=n;i>=1;i--) 
+    int r,o,i1,i2; 
+    int n=1; 
+    scanf("%d",&r); 
+    for(o=0;o<r;o++) 
     { 
-        for(j=1;j<=i;j++)
+        for(i1=r-1;i1>o;i1--)
+        {
+            printf(" ");
+        }
+        for(i2=0;i2<n;i2++)
         { 
-            printf("%c ",'A'-1 + i);
-        } 
+            printf("%d",n-o); 
+            
+        }
+        n+=2; 
         printf("
 ");
-    } return 0;
-    
+    }
 }
