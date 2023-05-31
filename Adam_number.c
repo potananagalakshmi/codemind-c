23579ae287e7750c141e26c12c0fd2b7af82e168
potananagalakshmi <119916338+potananagalakshmi@users.noreply.github.com>
2023-05-31 16:02:00 +0530

    #include<stdio.h> int main() {     int n,sum,m,x,temp,semp,rev=0;     scanf("%d",&n);     m=n*n;     while(n>0)     {         temp=n%10;         sum=(sum*10)+temp;         n=n/10;     }     x=sum*sum;     while(x>0)     {         semp=x%10;         rev=(rev*10)+semp;         x=x/10;     }     if(m==rev)     {         printf("True");     }     else     {         printf("False");     } }

--- Adam_number.c
+++ Adam_number.c
@@ -1,23 +1,23 @@
 #include<stdio.h>
 int main()
 {
-    int n,rem,rev=0,rem2,rev2=0;
+    int n,sum,m,x,temp,semp,rev=0;
     scanf("%d",&n);
-    int sq=n*n;
-    while(n!=0)
+    m=n*n;
+    while(n>0)
     {
-        rem=n%10;
-        rev=rev*10+rem;
+        temp=n%10;
+        sum=(sum*10)+temp; 
         n=n/10;
     }
-    int sq2=rev*rev;
-    while(sq2!=0)
+    x=sum*sum;
+    while(x>0)
     {
-        rem2=sq2%10;
-        rev2=rev2*10+rem2;
-        sq2=sq2/10;
+        semp=x%10;
+        rev=(rev*10)+semp;
+        x=x/10;
     }
-    if(rev2==sq)
+    if(m==rev)
     {
         printf("True");
     }
@@ -26,3 +26,5 @@ int main()
         printf("False");
     }
 }
+
+    
